c70929ba3b932740f04e92e16bc8527fc562b5b7
a-tsu <lab_is_truth@yahoo.co.jp>
2017-05-23 23:09:50 +0900

    fixed

--- toml/print.cc
+++ toml/print.cc
@@ -1,10 +1,13 @@
-#include <iostream>
 #include "cpptoml.h"
+
+#include <iostream>
+
+using namespace std;
 
 int main()
 {
     auto config = cpptoml::parse_file("config.toml");
-    std::string a = "テスト\n";
-    std::cout << a;
+    auto value = config->get_qualified_as<string>("first.key");
+    cout << "TEST";
     return 0;
 }
